8ad7e8676b91eb1e7261c6a37e4677cc6e89954e
Matt Radtke <more>
2020-03-27 13:01:28 -0500

    Add project files.

---
+++ Library-System/Library-System.cpp
@@ -0,0 +1,50 @@
+// Library-System.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+
+
+using namespace std;
+
+struct Name {
+	std::string first;
+	std::string last;
+};
+
+struct ContactInfo {
+	std::string email;
+	long phone;
+};
+
+struct Author {
+	int id;
+	Name name;
+};
+
+struct Member {
+	int id;
+	Name name;
+	ContactInfo info;
+};
+
+
+struct Book {
+	long ISBN;
+	vector<Author> authors;
+	int count;
+};
+
+
+struct BookLoan {
+	Member borrower;
+	Book book;
+	time_t dateBorrowed;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
