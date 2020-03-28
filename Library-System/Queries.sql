564710554f4a29b69e5c4fa3b593c0b1c3f4a979
Matt Radtke <more>
2020-03-27 20:45:51 -0500

    Updated queries and DB file

--- Library-System/Queries.sql
+++ Library-System/Queries.sql
@@ -1,19 +1,10 @@
-DROP TABLE IF EXISTS Books;
-DROP TABLE IF EXISTS Authors;
-DROP TABLE IF EXISTS Members;
-DROP TABLE IF EXISTS Contributors;
-DROP TABLE IF EXISTS Checkouts;
--- Test comment
--- Test comment 2
---Your comment is garbo
---jk gotem
 Create Table IF NOT EXISTS Books(
 	ISBN int,
 	Copies int check(Copies > 0),
 	Title varchar(50) not null,
 	Summary varchar(200),
 	Primary Key (ISBN)
-)
+);
 
 
 Create Table IF NOT EXISTS Authors (
@@ -21,37 +12,32 @@ Create Table IF NOT EXISTS Authors (
 	Name varchar(50) not null,
 	Address varchar(50),
 	Phone varchar(15)
-)
+);
 
 Create Table If Not Exists Members (
 	Id Integer primary key AUTOINCREMENT not null,
 	Name varchar(50) not null,
 	Address varchar(50),
-	Email varchar (50) check(Email like '%@%'),
+	Email varchar (50) check(Email like '%@%') unique,
 	Phone varchar(15)
-)
+);
 
-Create Table Contributors(
+Create Table If Not Exists Contributors(
 	BookISBN int,
 	AuthorId int,
 	Primary key (BookISBN, AuthorId),
 	Foreign Key (BookISBN) references Books,
 	Foreign Key (AuthorId) references Authors
-)
+);
 
 
 
-Create Table Checkouts(
+Create Table If Not Exists Checkouts(
 	BookISBN int,
 	MemberId int,
-	Counter int,
-	LoanDate DateTime,
-	DueDate DateTime,
-	Primary Key (BookISBN, MemberId, Counter), 
+	LoanDate DateTime not null,
+	DueDate DateTime not null,
+	Primary Key (BookISBN, MemberId), 
 	Foreign Key (BookISBN) references Books,
 	Foreign Key (MemberId) references Members
-)
-
-SELECT sql 
-FROM sqlite_master 
-WHERE name = 'albums'
+);
