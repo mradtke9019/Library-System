32440dcd6d4848bad0204922b953c4c55ada39dd
Matt Radtke <more>
2021-08-10 23:45:38 -0500

    Search update

--- Library-System-Web/Controllers/BooksController.cs
+++ Library-System-Web/Controllers/BooksController.cs
@@ -31,5 +31,29 @@ namespace Library_System_Web.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Search(string term, bool featured = false)
+        {
+            List<Book> books;
+            // Return the featured books
+            if (featured)
+            {
+                // Get all the 
+                List<int> ids = _librarySystemContext.FeaturedBooks.Select(x => x.BookIsbn).ToList();
+                books = ids.Select(x => _librarySystemContext.Books.Where(y => y.Isbn == x).First()).ToList();
+                return PartialView("_BookResults", books);
+            }
+
+            if (string.IsNullOrEmpty(term))
+                return null;
+            // Extremely innefficient if we have a large table. We are getting the entire table from the database
+            books = _librarySystemContext.Books.ToList();
+            var type = books.GetType().GetGenericArguments()[0];
+            var properties = type.GetProperties();
+            books = books.Where(x => properties.Any(p => p != null && p.GetValue(x) != null && p.GetValue(x).ToString().ToLower().Contains(term.ToLower()))).ToList();
+
+            return PartialView("_BookResults", books);
+        }
     }
 }
